0ce19dd088e8295aaa7ea75459535ec07e4984c7
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-01-06 19:16:10 -0500

    Basic Player movement established

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,6 @@
 extends CharacterBody2D
 @onready var anim_sprite = $AnimatedSprite2D
 
-func _ready() -> void:
-	setup_animations()
-
-func setup_animations():
-	#Create new SpriteFrames if necessary
-	var sprite_frames = anim_sprite.sprite_frames
-	if sprite_frames == null:
-		sprite_frames = SpriteFrames.new()
-		anim_sprite.sprite_frames = sprite_frames
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -21,12 +11,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Flip Sprite based on direction facing
+	#direction == 1,0,-1 (0 for not moving)
+	if direction > 0:
+		anim_sprite.flip_h = false
+	elif direction < 0:
+		anim_sprite.flip_h = true
+		
+	
+	#Play Animation
+	if direction == 0 :
+		anim_sprite.play("idle")
+	else:
+		#anim_sprite.play("start_run")
+		anim_sprite.play("run")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
